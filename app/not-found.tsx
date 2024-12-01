8bb29f547d6257c96e4d97f0cab57702200e76cb
Rijal Ghodi <rijalghodi10@gmail.com>
2024-12-01 18:03:32 +0700

    fix not found pade size

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+    <div className="min-h-screen w-screen flex items-center justify-center p-6">
       <div
         className={cn(
           '-z-10 fixed top-0 left-0 right-0 aspect-square bg-cover bg-center',
           "bg-[url('/icons/comet-dark.svg')]",
         )}
       ></div>
-      <div className="flex flex-col gap-11 max-w-96">
+      <div className="flex flex-col gap-11 max-w-96 w-full">
         <h1 className="text-center">Page Not Found</h1>
         <p className="text-center text-lg">
           Page not found We couldn’t find the page you were looking for. Check

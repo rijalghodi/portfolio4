43bdc1f0d4bdea22ad7b4805b3b9b33bc2132d7c
Rijal Ghodi <rijalghodi10@gmail.com>
2024-12-04 12:10:45 +0700

    fix loading page size

--- components/ui/loading-page.tsx
+++ components/ui/loading-page.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import { Loader } from './loader';
+import { useTheme } from '@/contexts/theme-context';
 
 export function LoadingPage() {
+  const { theme } = useTheme();
   return (
     <div className="min-h-screen w-screen flex items-center justify-center p-6">
       <div
         className={cn(
-          '-z-10 fixed left-0 right-0 top-1/2 -translate-y-1/2 aspect-square bg-cover bg-center',
-          "bg-[url('/icons/comet-dark.svg')]",
+          '-z-10 fixed left-1/2 -translate-x-1/2 w-full max-w-screen-md top-1/2 -translate-y-1/2 aspect-square bg-cover bg-center',
+          theme === 'dark'
+            ? "bg-[url('/icons/comet-dark.svg')]"
+            : "bg-[url('/icons/comet-light.svg')]",
         )}
       ></div>
       <div className="flex flex-col gap-11 max-w-96 w-full">

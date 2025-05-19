90f795b424d6d3169cbdcf090662a4141ddc3325
Rijal Ghodi <rijalghodi10@gmail.com>
2025-05-20 01:24:42 +0700

    fix type

--- app/sanity/[[...index]]/page.tsx
+++ app/sanity/[[...index]]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { sanityConfig } from "@/sanity.config";
+import sanityConfig from "@/sanity.config";
 import { NextStudio } from "next-sanity/studio";
 export default function AdminPage() {
   return <NextStudio config={sanityConfig} />;

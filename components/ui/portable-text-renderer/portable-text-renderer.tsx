9a0f4094657a0073b01a443f76497ba2754c3b5c
Rijal <rijalghodi.dev@gmail.com>
2025-11-11 06:47:51 +0700

    add semifooter toc

--- components/ui/portable-text-renderer/portable-text-renderer.tsx
+++ components/ui/portable-text-renderer/portable-text-renderer.tsx
@@ -194,9 +194,10 @@ const PortableTextRenderer: React.FC<PortableTextRendererProps> = ({
               In This Article
             </h2>
           </div>
-          <div className="flex-1 overflow-y-auto xl:h-[calc(100vh-200px)] px-4 pt-0 pb-4">
+          <div className="flex-1 overflow-y-auto xl:h-[calc(100vh-200px)] px-4">
             <TableOfContents content={value} />
           </div>
+          <div className="px-4 pb-4" />
         </div>
       )}
 

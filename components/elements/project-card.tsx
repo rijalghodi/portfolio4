205830c08f71d82fabcb0491c1baca5f7260dd5e
Rijal Ghodi <rijalghodi10@gmail.com>
2025-12-04 16:16:08 +0700

    min-h-72 in project card

--- components/elements/project-card.tsx
+++ components/elements/project-card.tsx
@@ -47,7 +47,7 @@ export function ProjectCard({
       {/* Image background */}
       <div
         className={cn(
-          "h-72 w-full rounded-2xl border overflow-hidden relative px-6 py-6 flex flex-col justify-end bg-background/50 hover:bg-muted/50 dark:bg-muted/50 dark:hover:bg-silent/50 backdrop-blur-md",
+          "min-h-72 w-full rounded-2xl border overflow-hidden relative px-6 py-6 flex flex-col justify-end bg-background/50 hover:bg-muted/50 dark:bg-muted/50 dark:hover:bg-silent/50 backdrop-blur-md",
         )}
         data-aos="fade-up"
         // style={{
